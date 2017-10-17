86110e2a0a2314b173037797eed0a28ef6878f26
gtandu <tbrandon91@hotmail.fr>
2017-10-17 21:22:02 +0200

    class user and authority for spring secutity #1

--- mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/model/User.java
+++ mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/model/User.java
@@ -1,5 +1,90 @@
 package fr.mediarollRest.mediarollRest.model;
 
-public class User {
+import java.util.Collection;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import fr.mediarollRest.mediarollRest.security.Authority;
+
+@Entity
+public class User implements UserDetails {
+	
+	@Id
+	private String mail;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "user")
+	protected Collection<Authority> authorities;
+	
+	public User() {
+		super();
+	}
+	
+	public User(String mail, String password, String firstName, String lastName) {
+		super();
+		this.mail = mail;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+	@Override
+	public String getUsername() {
+		return mail;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+	
 }
---
+++ mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/security/Authority.java
@@ -0,0 +1,70 @@
+package fr.mediarollRest.mediarollRest.security;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+@Entity
+public class Authority implements GrantedAuthority {
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private User user;
+
+	private String role;
+
+	public Authority() {
+	}
+
+	public Authority(String role) {
+		super();
+		this.role = role;
+	}
+
+	public Authority(User account, String role) {
+		super();
+		this.user = account;
+		this.role = role;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public User getAccount() {
+		return user;
+	}
+
+	public void setAccount(User account) {
+		this.user = account;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String getAuthority() {
+		return this.role;
+	}
+
+}
