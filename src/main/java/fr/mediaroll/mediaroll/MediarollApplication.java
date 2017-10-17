2ad33cfea9eb4e35d7762905c5269dbca69f729e
gtandu <tbrandon91@hotmail.fr>
2017-10-17 14:19:12 +0200

    Init project mediaroll

---
+++ src/main/java/fr/mediaroll/mediaroll/MediarollApplication.java
@@ -0,0 +1,12 @@
+package fr.mediaroll.mediaroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediarollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediarollApplication.class, args);
+	}
+}
