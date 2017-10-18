9e10c206cc753a2bdb9ba28b13f79a36eded72fd
gtandu <tbrandon91@hotmail.fr>
2017-10-18 10:33:43 +0200

    Refactoring service #8

--- mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/service/IUserService.java
+++ mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/service/IUserService.java
@@ -1,12 +1,12 @@
 package fr.mediarollRest.mediarollRest.service;
 
-import org.springframework.http.ResponseEntity;
-
 import fr.mediarollRest.mediarollRest.model.User;
 
 public interface IUserService {
 	
-	public ResponseEntity<User> findByMail(String mail);
+	public User findByMail(String mail);
+	
+	
 	
 
 }
