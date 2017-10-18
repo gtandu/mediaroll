bd717c16e7f811b114e298fd709cbc0f4af19d16
gtandu <tbrandon91@hotmail.fr>
2017-10-18 10:33:43 +0200

    Refactoring service #8

--- mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/service/implementation/UserService.java
+++ mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/service/implementation/UserService.java
@@ -1,10 +1,6 @@
 package fr.mediarollRest.mediarollRest.service.implementation;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import fr.mediarollRest.mediarollRest.model.User;
@@ -18,15 +14,9 @@ public class UserService implements IUserService {
 	private UserRepository userRepository;
 
 	@Override
-	public ResponseEntity<User> findByMail(String mail) {
-		Optional<User> userOptional = userRepository.findByMail(mail);
-		try {
-			User user = userOptional.get();
-			return new ResponseEntity<User>(user, HttpStatus.OK);
-		}
-		catch(Exception e) {
-			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
-		}
+	public User findByMail(String mail) {
+		return userRepository.findByMail(mail).get();
+		
 	}
 
 }
