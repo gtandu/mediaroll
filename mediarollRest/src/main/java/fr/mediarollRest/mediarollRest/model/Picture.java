90f678231b8ddbc1dd6dca8b90b088d786505ab0
cmapella <mapella.corentin@gmail.com>
2017-10-24 14:18:13 +0200

    Create class Picture #3

---
+++ mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/model/Picture.java
@@ -0,0 +1,9 @@
+package fr.mediarollRest.mediarollRest.model;
+
+public class Picture extends Media {
+
+	public Picture(int id, String name, String description, String importDate, User owner) {
+		super(id, name, description, importDate, owner);
+	}
+
+}
