92504cb4e46a0703947194fb6a40046d468ab3bf
gtandu <tbrandon91@hotmail.fr>
2017-11-19 20:35:04 +0100

    Login form with materialize css #17

--- mediarollClient/src/app/app.module.ts
+++ mediarollClient/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { LoginFormComponent } from './login-form/login-form.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- mediarollClient/src/app/login-form/login-form.component.ts
+++ mediarollClient/src/app/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    console.log(this.model);
   }
 
 }
