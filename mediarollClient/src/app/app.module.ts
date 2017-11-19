347af5ae7a7b414f4aebf7a28048af228893bf4b
gtandu <tbrandon91@hotmail.fr>
2017-11-19 18:56:53 +0100

    Create route and login-form component #17

--- mediarollClient/src/app/app.module.ts
+++ mediarollClient/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
