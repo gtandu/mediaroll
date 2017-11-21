0fae97bd28c03f830d3eb654477998d6cb5c001b
gtandu <tbrandon91@hotmail.fr>
2017-11-21 17:23:40 +0100

    Add guard to mediaroll module + create home module + rediction to home if login success

--- mediarollClient/src/app/models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    mail:String;
-    password:String;
-
-    constructor(private model:any){
-        this.mail = model.email;
-        this.password = model.password;
-    }
-}
