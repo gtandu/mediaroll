6b9ab3a368d7fa13a54574fc034c03edc368bd65
gtandu <tbrandon91@hotmail.fr>
2017-12-18 11:28:04 +0100

    Fix build

--- mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/service/implementation/MediaManagerService.java
+++ mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/service/implementation/MediaManagerService.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -103,7 +104,7 @@ public class MediaManagerService implements IMediaManagerService {
 	}
 
 	@Override
-	public BufferedInputStream getInputStreamFromMedia(String filePath) throws MediaNotFoundException{
+	public InputStream getInputStreamFromMedia(String filePath) throws MediaNotFoundException{
 		try {
 			return new FileInputStream(filePath);
 		} catch (FileNotFoundException e) {
