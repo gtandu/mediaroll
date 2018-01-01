61656bf737a9151c7c15c6342af51a82e3456dcc
gtandu <tbrandon91@hotmail.fr>
2018-01-01 22:27:04 +0100

    Update class Video

--- mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/model/Video.java
+++ mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/model/Video.java
@@ -5,19 +5,7 @@ import javax.persistence.Entity;
 @Entity
 public class Video extends Media {
 
-	private String duration;
-
 	public Video() {
 		super();
-		this.duration = "";
 	}
-
-	public String getDuration() {
-		return duration;
-	}
-
-	public void setDuration(String duration) {
-		this.duration = duration;
-	}
-
 }
