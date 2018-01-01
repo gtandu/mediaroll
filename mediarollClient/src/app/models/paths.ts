19c8eeb9ab24d7ad0e3f52327334b97c7ac5ce7b
gtandu <tbrandon91@hotmail.fr>
2018-01-01 22:24:51 +0100

    Add service to media service

--- mediarollClient/src/app/models/paths.ts
+++ mediarollClient/src/app/models/paths.ts
@@ -5,5 +5,7 @@ export class Paths{
     public static readonly MEDIA_ID = '/{mediaId}';
     public static readonly MEDIAS_WITH_ID = Paths.MEDIAS + Paths.MEDIA_ID;
     public static readonly MEDIA_RESPONSE = Paths.MEDIAS_WITH_ID + '/response';
+    public static readonly PICTURES = Paths.MEDIAS + '/pictures';
+    public static readonly VIDEOS = Paths.MEDIAS + '/videos';
 
 }
--- mediarollClient/src/app/services/media/media.service.ts
+++ mediarollClient/src/app/services/media/media.service.ts
@@ -3,6 +3,8 @@ import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { HttpHeaders, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import { AuthentificationService } from '../authentification.service';
 import { Media } from '../../models/media';
+import { Picture } from '../../models/picture';
+import { Video } from '../../models/video';
 import { Paths } from '../../models/paths';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
@@ -11,16 +13,26 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class MediaService {
 
-  constructor(private http: Http, private authService: AuthentificationService) { }
+  headers = new Headers({ 'Authorization': 'Token ' + this.authService.token });
+  options = new RequestOptions({ headers: this.headers });
 
-  getAllMedias(): Observable<Media[]> {
-            const headers = new Headers({ 'Authorization': 'Token ' + this.authService.token});
-            const options = new RequestOptions({ headers: headers });
-            return this.http.get(this.authService.server + Paths.MEDIAS, options)
-                .map((response: Response) => response.json());
+  constructor(private http: Http, private authService: AuthentificationService) {
   }
 
-  buildUrlToMediaWithId(id: string){
+  getAllMedias(): Observable<Media[]> {
+    return this.http.get(this.authService.server + Paths.MEDIAS, this.options)
+      .map((response: Response) => response.json());
+  }
+
+  getAllPictures(): Observable<Picture[]> {
+    return this.http.get(this.authService.server + Paths.PICTURES, this.options).map((response: Response) => response.json());
+  }
+
+  getAllVideos(): Observable<Video[]> {
+    return this.http.get(this.authService.server + Paths.VIDEOS, this.options).map((response: Response) => response.json());
+  }
+
+  buildUrlToMediaWithId(id: string) {
     return this.authService.server + Paths.MEDIAS + '/' + id + '/response';
   }
 
