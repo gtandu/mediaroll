f5cb2d83fc6293fc93c29b0ebf12c5380f45ea8c
gtandu <tbrandon91@hotmail.fr>
2018-01-01 23:10:26 +0100

    Add filter media

--- mediarollClient/src/app/mediaroll/sidebar/sidebar.component.ts
+++ mediarollClient/src/app/mediaroll/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
@@ -6,10 +6,30 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewChecked {
 
   ngOnInit() {
+  }
 
+  ngAfterViewChecked() {
+    this.filterPhotos();
+    this.filterVideos();
+  }
+
+  filterPhotos() {
+    $('#filterPhotos').click(function(e){
+      e.preventDefault();
+      $('.mediaroll-videos').hide();
+      $('.mediaroll-pictures').show();
+    });
+  }
+
+  filterVideos() {
+    $('#filterVideos').click(function(e){
+      e.preventDefault();
+      $('.mediaroll-videos').show();
+      $('.mediaroll-pictures').hide();
+    });
   }
 
 }
