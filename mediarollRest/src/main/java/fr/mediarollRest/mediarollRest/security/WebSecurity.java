e2a5e804b6aba05c3092c5db8a3c79e0fdeae31e
gtandu <tbrandon91@hotmail.fr>
2018-11-22 22:56:54 +0100

    Allow CORS *

--- mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/security/WebSecurity.java
+++ mediarollRest/src/main/java/fr/mediarollRest/mediarollRest/security/WebSecurity.java
@@ -58,7 +58,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	    
 	    CorsConfiguration configAutenticacao = new CorsConfiguration();
 	    configAutenticacao.setAllowCredentials(true);
-	    configAutenticacao.addAllowedOrigin("http://localhost:4200");
+	    configAutenticacao.addAllowedOrigin("*");
 	    configAutenticacao.addAllowedHeader("Authorization");
 	    configAutenticacao.addAllowedHeader("Content-Type");
 	    configAutenticacao.addAllowedHeader("Accept");
